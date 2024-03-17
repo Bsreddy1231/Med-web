f75f5bd7eb70cc9e378333295fc18f0f2dbdcde9
Bsreddy1231 <142500049+Bsreddy1231@users.noreply.github.com>
2024-03-17 15:02:01 +0530

    Update code.py

--- Virtual Healthcare/code.py
+++ Virtual Healthcare/code.py
@@ -16,7 +16,7 @@ import streamlit as st
 # Reading the train.csv by removing the
 # last column since it's an empty column
 warnings.filterwarnings("ignore")
-DATA_PATH = r"C:\Users\HP\OneDrive\Desktop\project\Training.csv"
+DATA_PATH = r"C:\Users\91630\Downloads\Med-web-main\Med-web-main\Training.csv"
 data = pd.read_csv(DATA_PATH).dropna(axis = 1)
 
 # Checking whether the dataset is balanced or not
@@ -47,79 +47,6 @@ models = {
     "Gaussian NB":GaussianNB(),
     "Random Forest":RandomForestClassifier(random_state=18)
 }
-# Producing cross validation score for the models
-for model_name in models:
-    model = models[model_name]
-    scores = cross_val_score(model, X, y, cv = 10,
-                             n_jobs = -1,
-                             scoring = cv_scoring)
-# Training and testing SVM Classifier
-svm_model = SVC()
-svm_model.fit(X_train, y_train)
-preds = svm_model.predict(X_test)
-
-cf_matrix = confusion_matrix(y_test, preds)
-plt.figure(figsize=(12,8))
-sns.heatmap(cf_matrix, annot=True)
-plt.title("Confusion Matrix for SVM Classifier on Test Data")
-
-# Training and testing Naive Bayes Classifier
-nb_model = GaussianNB()
-nb_model.fit(X_train, y_train)
-preds = nb_model.predict(X_test)
-# Importing libraries
-import numpy as np
-import pandas as pd
-import warnings
-from scipy.stats import mode
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.svm import SVC
-from sklearn.naive_bayes import GaussianNB
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix
-import streamlit as st
-
-
-# matplotlib inline
-# Reading the train.csv by removing the
-# last column since it's an empty column
-warnings.filterwarnings("ignore")
-DATA_PATH = (r"C:\Users\ASUS\OneDrive\Desktop\H!\ml_model\Training.csv")
-data = pd.read_csv(DATA_PATH).dropna(axis = 1)
-
-# Checking whether the dataset is balanced or not
-disease_counts = data["prognosis"].value_counts()
-temp_df = pd.DataFrame({
-    "Disease": disease_counts.index,
-    "Counts": disease_counts.values
-})
-
-plt.figure(figsize = (18,8))
-sns.barplot(x = "Disease", y = "Counts", data = temp_df)
-plt.xticks(rotation=90)
-# Encoding the target value into numerical
-# value using LabelEncoder
-encoder = LabelEncoder()
-data["prognosis"] = encoder.fit_transform(data["prognosis"])
-X = data.iloc[:,:-1]
-y = data.iloc[:, -1]
-X_train, X_test, y_train, y_test =train_test_split(
-  X, y, test_size = 0.2, random_state = 24)
-
-# Defining scoring metric for k-fold cross validation
-def cv_scoring(estimator, X, y):
-    return accuracy_score(y, estimator.predict(X))
-
-# Initializing Models
-models = {
-    "SVC":SVC(),
-    "Gaussian NB":GaussianNB(),
-    "Random Forest":RandomForestClassifier(random_state=18)
-}
-
 # Producing cross validation score for the models
 for model_name in models:
     model = models[model_name]
@@ -166,7 +93,7 @@ final_nb_model.fit(X, y)
 final_rf_model.fit(X, y)
 
 # Reading the test data
-test_data = pd.read_csv(r"C:\Users\ASUS\OneDrive\Desktop\H!\ml_model\Testing.csv").dropna(axis=1)
+test_data = pd.read_csv(r"C:\Users\91630\Downloads\Med-web-main\Med-web-main\Testing.csv").dropna(axis=1)
 
 test_X = test_data.iloc[:, :-1]
 test_Y = encoder.transform(test_data.iloc[:, -1])
@@ -224,7 +151,7 @@ def predictDisease(symptoms):
     st.write('You may be suffering from: ' ,final_prediction)
 
 
-st.title("MED-WEB")
+st.title("VIRTUAL HEALTHCARE")
 st.header("Doctor At Home")
 
 sym1 =st.text_input("Enter the 1st symptom: ")
